f1fd959ce035bbdb5432060ee50c366d47e335e2
Thomas Nürk <nurkthomas@gmail.com>
2022-12-06 09:59:54 +0100

    Added basic navigation bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,11 +11,12 @@ export default function RootLayout({
       <html>
       <body>
       <main>
-          <nav className={"font-bold underline"}>
-              <Link href="/">
+          <div className="flex bg-gray-100 justify-between">
+              <div className="text-gray-800 bg-gray-300 px-4 py-2 m-2 float-right">Office-Sharing</div>
+              <Link href={"/"} className="text-gray-800 bg-gray-300 px-4 py-2 m-2 float-left">
                   Abmelden
               </Link>
-          </nav>
+          </div>
           {children}
       </main>
       </body>
