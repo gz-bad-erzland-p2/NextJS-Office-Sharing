b61f3db98286d6a0e386274165b46d0d55ab5b49
Thomas Nürk <nurkthomas@gmail.com>
2022-12-06 09:37:39 +0100

    Removed unused css

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,14 +9,11 @@ export default function RootLayout({
 }) {
   return (
       <html>
-      <body className={"p-8"}>
-      <main className={"p-4 flex flex-col content-start items-start"}>
-          <nav className={"font-bold m-2 underline"}>
+      <body>
+      <main>
+          <nav className={"font-bold underline"}>
               <Link href="/">
-                  Home
-              </Link>
-              <Link href="/notes">
-                  Notes
+                  Abmelden
               </Link>
           </nav>
           {children}
