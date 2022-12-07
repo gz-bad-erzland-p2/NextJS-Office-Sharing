2cf48659aac0492df1279bb47efb676a8db9c6fd
Xmg-Privat-Ubuntu <nurkthomas@gmail.com>
2022-12-07 22:14:51 +0100

    Changed header flex box to list

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,11 +11,15 @@ export default function RootLayout({
       <html>
       <body>
       <main>
-          <div className="flex bg-gray-100 justify-between">
-              <div className="text-gray-800 bg-gray-300 px-4 py-2 m-2 float-right">Office-Sharing</div>
-              <Link href={"/"} className="text-gray-800 bg-gray-300 px-4 py-2 m-2 float-left">
-                  Abmelden
-              </Link>
+          <div className="relative w-full float-left bg-gray-100 mb-5">
+              <div className="text-gray-800 px-4 py-2 m-2 float-left">Office-Sharing Bad-Erzland</div>
+              <ul className={"relative list-none "}>
+                  <li className={"float-right text-gray-800 bg-gray-300 px-4 py-2 m-2 block"}>
+                      <Link href={"/"}>
+                          Abmelden
+                      </Link>
+                  </li>
+              </ul>
           </div>
           {children}
       </main>
