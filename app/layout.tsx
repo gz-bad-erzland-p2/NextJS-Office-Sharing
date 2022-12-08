00612d74afb2be0dc261140f45cbceb65e1e3697
derech1e <39240736+derech1e@users.noreply.github.com>
2022-12-08 07:29:44 +0100

    Landing page (#2)
    * Added colors to tailwind.config.js
    
    * Added landing page
    
    * Fixed image sizing
    
    * enhanced icon import

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,8 +12,8 @@ export default function RootLayout({
       <body>
       <main>
           <div className="flex bg-gray-100 justify-between">
-              <div className="text-gray-800 bg-gray-300 px-4 py-2 m-2 float-right">Office-Sharing</div>
-              <Link href={"/"} className="text-gray-800 bg-gray-300 px-4 py-2 m-2 float-left">
+              <div className="text-gray-800 bg-gray-300 px-4 py-2 m-2">Office-Sharing</div>
+              <Link href={"/"} className="text-gray-800 bg-gray-300 px-4 py-2 m-2">
                   Abmelden
               </Link>
           </div>
