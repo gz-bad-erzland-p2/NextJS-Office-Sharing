ea0fe61afe8f20b0890e9c2ac5aa70de60ec2d55
Thomas Nürk <nurkthomas@gmail.com>
2022-12-09 12:38:03 +0100

    Finished WizardContext

--- app/wizard/page.tsx
+++ app/wizard/page.tsx
@@ -1,6 +1,36 @@
+"use client";
+
+import {DefaultWizardStepProps, Step, Steps, useWizardContext, WizardStepProvider} from "../../context/WizardContext";
+
+
+
 export default function WizardMainPage() {
+
+    const { activeStepIndex, onNext, onPrevious, goTo, isFirstStep, isLastStep } =
+        useWizardContext<DefaultWizardStepProps>();
+
+    const WizardStepCustom = () => {
+        const {activeStepIndex} = useWizardContext<DefaultWizardStepProps>();
+
+        return (
+            <Steps>
+                <Step key={`page/1`} id={'1'}>
+                    <div>
+                        Test Page 1
+                    </div>
+                </Step>
+                <Step key={`page/2`} id={'2'}>
+                    <div>
+                        Test Page 2
+                    </div>
+                </Step>
+            </Steps>
+        );
+    };
+
     return (
-      <div>
-      </div>
+        <WizardStepProvider>
+            <WizardStepCustom />
+        </WizardStepProvider>
     );
-}
+    }
