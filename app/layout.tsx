663115d8a666c03efcfd4630b6378148699c8cd4
JannikHein <118260038+JannikHein@users.noreply.github.com>
2022-12-12 15:00:57 +0100

    font disabled

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,14 +3,14 @@ import './global.css'
 
 import HeaderComponent from "../components/page/HeaderComponent";
 import FooterComponent from "../components/page/FooterComponent";
-import { Inter } from '@next/font/google';
+/*import { Inter } from '@next/font/google';
 
 const inter = Inter({
     variable: '--font-inter',
-});
+});*/
 export default function RootLayout({children}: { children: React.ReactNode }) {
     return (
-        <html lang={"de"} className={inter.className}>
+        <html lang={"de"} /*className={inter.className}*/>
         <body>
         {/* See https://www.thefullstackblog.com/how-to-make-the-footer-start-at-the-bottom-of-the-screen-with-tailwind-css  */}
         <main className={"min-h-screen flex flex-col min-w-0"}>
