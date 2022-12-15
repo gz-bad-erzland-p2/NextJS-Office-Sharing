2e22dbe9639e80e7235fe0ca3ed846d78bb836de
derech1e <nurkthomas@gmail.com>
2022-12-15 16:41:19 +0100

    Tweak footer and wizard stepper

--- components/page/FooterComponent.tsx
+++ components/page/FooterComponent.tsx
@@ -1,70 +1,23 @@
+import Link from "next/link";
+
 export default function FooterComponent() {
 
     return (
-        // <div className="flex-wrap justify-between items-center">
-
-            <div className="bg-white sticky p-5" style={{top: '100vh'}}>
-                <div className="mx-auto max-w-screen-xl">
-                <div className="md:flex md:justify-between">
-                    {/*<div className="mb-6 md:mb-0">*/}
-                    {/*    <a href="https://flowbite.com/" className="flex items-center">*/}
-                    {/*        <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-8"*/}
-                    {/*             alt="FlowBite Logo"/>*/}
-                    {/*        <span*/}
-                    {/*            className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>*/}
-                    {/*    </a>*/}
-                    {/*</div>*/}
-                    <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
-                        <div>
-                            <h2 className="mb-6 text-sm font-semibold  uppercase text-gray-700">Links</h2>
-                            <ul className="text-gray-600 dark:text-gray-400">
-                                <li className="mb-4">
-                                    <a href="/" className="hover:underline">Home</a>
-                                </li>
-                                <li className="mb-4">
-                                    <a href="/hardware" className="hover:underline">Hardware</a>
-                                </li>
-                                <li>
-                                    <a href="/support" className="hover:underline">Support</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <div>
-                            <h2 className="mb-6 text-sm font-semibold  uppercase text-gray-700">Anmeldung</h2>
-                            <ul className="text-gray-600 dark:text-gray-400">
-                                <li className="mb-4">
-                                    <a href="https://github.com/themesberg/flowbite"
-                                       className="hover:underline ">Anmelden</a>
-                                </li>
-                                <li className="mb-4">
-                                    <a href="https://discord.gg/4eeurUVvTy" className="hover:underline">Registrieren</a>
-                                </li>
-                                <li>
-                                    <a href="https://discord.gg/4eeurUVvTy" className="hover:underline">Ausloggen</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <div>
-                            <h2 className="mb-6 text-sm font-semibold  uppercase text-gray-700">Rechtsschutz</h2>
-                            <ul className="text-gray-600 dark:text-gray-400">
-                                <li className="mb-4">
-                                    <a href="#" className="hover:underline">Impressum</a>
-                                </li>
-                                <li >
-                                    <a href="#" className="hover:underline">Datenschutz</a>
-                                </li>
-                            </ul>
-                        </div>
+        <div className="bg-white sticky p-2.5" style={{top: '100vh'}}>
+            <div className="mx-auto max-w-screen-xl">
+                <div className="flex items-center justify-between">
+                    <span
+                        className="text-sm sm:text-center text-gray-400">© 2022 <Link
+                        href="#" className="hover:underline">SecNetIT</Link>. All Rights Reserved.
+                    </span>
+                    <div className={"flex flex-col md:flex-row gap-2 justify-between text-gray-400"}>
+                        <Link href="#"
+                              className="hover:underline">Impressum</Link>
+                        <Link href="#"
+                              className="hover:underline">Datenschutz</Link>
                     </div>
                 </div>
-                <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8"/>
-                <div className="sm:flex sm:items-center sm:justify-between">
-                    <span className="text-sm  sm:text-center dark:text-gray-400">© 2022 <a
-                        href="https://flowbite.com/" className="hover:underline">SecNetIT</a>. All Rights Reserved.        </span>
-                    
-                </div>
-                </div>
             </div>
-        // </div>
+        </div>
     );
 }
--- components/page/HeaderComponent.tsx
+++ components/page/HeaderComponent.tsx
@@ -18,11 +18,11 @@ export default function HeaderComponent() {
 
         <nav className="bg-white border-gray-200 px-2 md:px-4 py-2.5 drop-shadow-md z-50">
             <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-                <Link href="https://flowbite.com" className="flex items-center">
+                <Link href="#" className="flex items-center">
                     {/*<img className="mr-3 h-6 sm:h-9"*/}
                     {/*     alt="Flowbite Logo"/>*/}
                     <span
-                        className="self-center text-xl font-semibold whitespace-nowrap ">Office Sharing</span>
+                        className="self-center text-xl font-semibold whitespace-nowrap">Office Sharing</span>
                 </Link>
                 <div className="flex items-center md:order-2">
                     <Link href="/login"
