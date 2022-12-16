c66c66f3b492b4469ada9b45063045f28069d177
Thomas Nürk <nurkthomas@gmail.com>
2022-12-16 12:09:05 +0100

    Fix #19

--- components/wizard/TimeSlotSelectorComponent.tsx
+++ components/wizard/TimeSlotSelectorComponent.tsx
@@ -1,31 +1,34 @@
-export default function TimeSlotSelectorComponent(){
+export default function TimeSlotSelectorComponent() {
 
-    return(
+    return (
         <div className="flex justify-between gap-4">
 
-                                <div>
-                                    <h2>Mietdatum:</h2>
-                                    <input className="bg-white rounded-lg w-[252px] h-[40px] p-2 border-2 text-center" type="date" id="start" name="trip-start"
-                                    value="2022-07-22"></input>
-                                </div>
-                                <div>
-                                <h2>Startzeit auswählen:</h2>
-                                <select className="bg-white rounded-lg w-[252px] h-[40px] p-2 border-2 text-center" name="cars" id="cars">
-                                    <option value="10:00">10:00</option>
-                                    <option value="11:00">11:00</option>
-                                    <option value="12:00">13:00</option>
-                                    <option value="13:00">14:00</option>
-                                </select>
-                                <h2>Endzeit auswählen:</h2>
-                                <select className="bg-white rounded-lg w-[252px] h-[40px] p-2 border-2 text-center"  name="cars" id="cars">
-                                    <option value="10:00">10:00</option>
-                                    <option value="11:00">11:00</option>
-                                    <option value="12:00">13:00</option>
-                                    <option value="13:00">14:00</option>
-                                </select>
+            <div>
+                <h2>Mietdatum:</h2>
+                <input className="bg-white rounded-lg w-[252px] h-[40px] p-2 border-2 text-center" type="date"
+                       id="start" name="trip-start"
+                       value="2022-07-22"></input>
+            </div>
+            <div>
+                <h2>Startzeit auswählen:</h2>
+                <select className="bg-white rounded-lg w-[252px] h-[40px] p-2 border-2 text-center" name="cars"
+                        id="cars">
+                    <option defaultValue="10:00">10:00</option>
+                    <option defaultValue="11:00">11:00</option>
+                    <option defaultValue="12:00">13:00</option>
+                    <option defaultValue="13:00">14:00</option>
+                </select>
+                <h2>Endzeit auswählen:</h2>
+                <select className="bg-white rounded-lg w-[252px] h-[40px] p-2 border-2 text-center" name="cars"
+                        id="cars">
+                    <option defaultValue="10:00">10:00</option>
+                    <option defaultValue="11:00">11:00</option>
+                    <option defaultValue="12:00">13:00</option>
+                    <option defaultValue="13:00">14:00</option>
+                </select>
 
-                                </div>
-                            </div>
+            </div>
+        </div>
 
     )
 }
