47f92d6ea241e86a92b13865fdcb1aa2c8bc43fc
derech1e <39240736+derech1e@users.noreply.github.com>
2023-01-03 10:14:19 +0100

    Add wizard state context (#25)
    * Updated packages
    
    * Add wizard state context
    This context allow to preserve the user input data over multiple wizard pages
    
    * Add enums for wizard state context

---
+++ core/utils/enums/OperatingSystemEnum.tsx
@@ -0,0 +1,12 @@
+// export const OperatingSystemEnum: { [x: string]: 'WINDOWS' | 'LINUX'} = {
+//     WINDOWS: 'WINDOWS',
+//     LINUX: 'LINUX'
+// }
+//
+//
+// export type OperatingSystemEnum = typeof OperatingSystemEnum[keyof typeof OperatingSystemEnum]
+
+export enum OperatingSystemEnum {
+    WINDOWS = "WINDOWS",
+    LINUX = "LINUX"
+}
---
+++ core/utils/enums/WorkspaceTypeEnum.tsx
@@ -0,0 +1,4 @@
+export enum WorkspaceTypeEnum {
+    DOUBLE_DESK = "DOUBLE_DESK",
+    SINGLE_DESK = "SINGLE_DESK"
+}
