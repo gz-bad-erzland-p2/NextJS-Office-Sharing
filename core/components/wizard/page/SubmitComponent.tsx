1f6ca7cd25840d10888de135b0295a51c85ffb4a
derech1e <nurkthomas@gmail.com>
2023-01-10 16:12:38 +0100

    Cleanup last commit

--- core/components/wizard/page/SubmitComponent.tsx
+++
@@ -1,9 +0,0 @@
-import PdfLinkComponent from "./pdf/PdfLinkComponent";
-
-export const SubmitComponent = () => {
-    return (
-        <div className={"h-full w-full"}>
-            <PdfLinkComponent fileName={"test.pdf"}/>
-        </div>
-    );
-}
