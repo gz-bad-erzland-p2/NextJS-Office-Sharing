8c76f1d6c53779763fb8518240714b0f8c756a29
JannikHein <118260038+JannikHein@users.noreply.github.com>
2023-01-20 13:27:51 +0100

    Update select option component

--- core/components/wizard/page/HardwareSelectionComponent.tsx
+++ core/components/wizard/page/HardwareSelectionComponent.tsx
@@ -12,79 +12,60 @@ import laptop from "../../../../public/assets/svg/laptop.svg";
 import devices from "../../../../public/assets/svg/devices.svg";
 
 import { WorkspaceTypeEnum } from "../../../utils/enums/WorkspaceTypeEnum";
+import SelectOption from "../SelectOption";
 
 export const HardwareSelectionComponent = () => {
     const { hardware, hardware2, setHardware, setHardware2, workspaceType } = useWizardStateContext();
 
+    const hardwareList = [
+        {
+            name: "Laptop",
+            img:"../../../assets/svg/laptop.svg",
+            category:"hardware",
+            value:HardwareEnum.Laptop,
+            info: "hardware",
+            price:"12€"
+
+    },
+        {
+            name: "PC",
+            img:"../../../assets/svg/desktop.svg",
+            category:"hardware",
+            value:HardwareEnum.PC
+    },
+        {
+            name: "Barebone",
+            img:"../../../assets/svg/barebone.svg",
+            category:"hardware",
+            value:HardwareEnum.BAREBONE
+    },
+        {
+            name: "Bringe eigenes Gerät",
+            img:"../../../assets/svg/devices.svg",
+            category:"hardware",
+            value:HardwareEnum.BYOD
+    },
+
+]
 
     return (
         <form id={"form"} className={"h-full w-full"}>
             <h3 className="mb-5 text-lg font-medium text-red ">Benötigen sie einen Rechner?</h3>
             <ul className="flex md:flex-row flex-col min-w-0 gap-2  w-full items-center justify-center">
-                <li className={"w-full h-10 md:h-40"} onClick={() => {
-                    setHardware(HardwareEnum.PC);
-                }}>
-                    <input type="radio" id="PC" name="it-system"
-                        className="hidden peer" required
+                
 
-                        defaultChecked={hardware === HardwareEnum.PC} />
+                {
+                    hardwareList.map((item, index) => {
+                        return (
+                            //<div>{item.name}</div>
+                            <SelectOption key={item.name + "-" + index} setter={setHardware} getter={hardware} name ={item.name} img={item.img} value={item.img} category={item.category} infoUrl={item.info} price={item.price}/>
+                        );
+                    })
+                }
+                
 
-                    <label htmlFor="PC"
-                        className="flex flex-col items-center w-full h-full text-gray-500 bg-white rounded-lg border-2 border-gray-200 cursor-pointer peer-checked:border-office-green-500 peer-checked:text-office-green-500 hover:text-gray-600 hover:bg-gray-100">
-                        <div
-                            className={"flex flex-row md:flex-col w-full h-full items-center"}>
-                            <Image src={desktop}
-                                alt={"Bild Doppelarbeitsplatz"}
-                                className={"flex-grow md:w-full h-full !max-w-[90px] md:max-w-none"} />
-                            <div className="text-lg font-semibold">
-                                PC
-                            </div>
-                        </div>
-                    </label>
-                </li>
-                <li className={"w-full h-10 md:h-40"} onClick={() => {
-                    setHardware(HardwareEnum.Laptop);
-                }}>
-                    <input type="radio" id="laptop" name="it-system"
-                        className="hidden peer"
-
-                        defaultChecked={hardware === HardwareEnum.Laptop} />
-
-                    <label htmlFor="laptop"
-                        className="inline-flex justify-center items-center h-full w-full text-gray-500 bg-white rounded-lg border-2 border-gray-200 cursor-pointer peer-checked:border-office-green-500 peer-checked:text-office-green-500 hover:text-gray-600 hover:bg-gray-100">
-                        <div
-                            className={"flex flex-row md:flex-col w-full h-full items-center justify-left md:justify-between"}>
-                            <Image src={laptop}
-                                alt={"Bild Doppelarbeitsplatz"}
-                                className={"flex-grow md:w-full h-full !max-w-[90px] md:max-w-none"} />
-                            <div className="text-lg font-semibold">
-                                Laptop
-                            </div>
-                        </div>
-                    </label>
-                </li>
-
-                <li className={"w-full h-10 md:h-40"} onClick={() => {
-                    setHardware(HardwareEnum.BYOD);
-                }}>
-                    <input type="radio" id="byod" name="it-system"
-                        className="hidden peer"
-
-                        defaultChecked={hardware === HardwareEnum.BYOD} />
-
-                    <label htmlFor="byod"
-                        className="inline-flex justify-center items-center h-full w-full text-gray-500 bg-white rounded-lg border-2 border-gray-200 cursor-pointer peer-checked:border-office-green-500 peer-checked:text-office-green-500 hover:text-gray-600 hover:bg-gray-100">
-                        <div
-                            className={"flex flex-row md:flex-col  w-full h-full items-center justify-left md:justify-between"}>
-                            <Image src={devices}
-                                alt={"Bild Doppelarbeitsplatz"}
-                                className={"flex-grow md:w-full h-full !max-w-[90px] md:max-w-none"} />
-                            <div className="text-lg font-semibold">
-                                Bringe eigenes Gerät
-                            </div>
-                        </div>
-                    </label>
-                </li>
+                
+                
 
             </ul>
             {workspaceType == WorkspaceTypeEnum.DOUBLE_DESK &&
@@ -95,7 +76,7 @@ export const HardwareSelectionComponent = () => {
                         <li className={"w-full h-10 md:h-40"} onClick={() => {
                             setHardware2(HardwareEnum.PC);
                         }}>
-                            <input type="radio" id="PC2" name="it-system2"
+                            <input type="radio" id="PC2" name="hardware2"
                                 className="hidden peer" required
 
                                 defaultChecked={hardware2 === HardwareEnum.PC} />
@@ -116,7 +97,7 @@ export const HardwareSelectionComponent = () => {
                         <li className={"w-full h-10 md:h-40"} onClick={() => {
                             setHardware2(HardwareEnum.Laptop);
                         }}>
-                            <input type="radio" id="laptop2" name="it-system2"
+                            <input type="radio" id="laptop2" name="hardware2"
                                 className="hidden peer"
 
                                 defaultChecked={hardware2 === HardwareEnum.Laptop} />
@@ -137,7 +118,7 @@ export const HardwareSelectionComponent = () => {
                         <li className={"w-full h-10 md:h-40"} onClick={() => {
                             setHardware2(HardwareEnum.BYOD);
                         }}>
-                            <input type="radio" id="byod2" name="it-system2"
+                            <input type="radio" id="byod2" name="hardware2"
                                 className="hidden peer"
                                 defaultChecked={hardware2 === HardwareEnum.BYOD} />
 
