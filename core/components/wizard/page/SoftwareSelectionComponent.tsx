f3d1fe7cd5965e1dc49f4fdc566a99e5bcd93912
JannikHein <118260038+JannikHein@users.noreply.github.com>
2023-01-24 11:47:56 +0100

    Update vorinstalierte software

--- core/components/wizard/page/SoftwareSelectionComponent.tsx
+++ core/components/wizard/page/SoftwareSelectionComponent.tsx
@@ -14,7 +14,7 @@ export const SoftwareSelectionComponent = () => {
               className={"w-full h-full flex flex-col justify-center"}>
             <label htmlFor="message"
                    className="block mb-2 text-sm font-medium text-gray-900">Haben
-                sie weitere Anforderungen?</label>
+                sie weitere Anforderungen?</label><br></br> <a className="text-xs">(Bereits vorinstaliert Firefox, Chrome, LibreOffice, Gimp2, Discord, AdobeAcrobatReader, VLC Mediaplayer)</a>
             <textarea id="message" rows={4}
                       className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300"
                       placeholder="Weitere Spezifikationen, Betriebssystem,..."
