307573cfd4eaebcb82fc330b27fd07e0d0ba89f0
JannikHein <118260038+JannikHein@users.noreply.github.com>
2023-01-27 13:28:12 +0100

    Add TimeCalculation notfinal

--- core/utils/CostUtils.tsx
+++ core/utils/CostUtils.tsx
@@ -30,3 +30,36 @@ export function calculateCost(wizardStateContext: DefaultWizardStateProps): numb
     }
     return cost;
 }
+
+
+export function calculateTime(wizardStateContext: DefaultWizardStateProps){
+     
+    const openingTime = 7;
+    const closingTime = 20;
+
+    var hoursFinal = 0; //final time
+
+    const start = wizardStateContext.startDate;
+    const end = wizardStateContext.endDate;
+
+    const hoursTemp = Math.abs(start.getTime() - end.getTime()) / 36e5;
+
+    if(hoursTemp <= closingTime -openingTime){
+        hoursFinal = hoursTemp;
+    }else{
+        const startTime = (start.getTime()-start.getDate()) / 36e5;
+        const endTime = (end.getTime()-end.getDate()) / 36e5;
+        const daysBetween = Math.ceil((start.getTime() - end.getTime())  / (1000 * 3600 * 24)) -1;//TODO anzahl tage dazwischen richtig berechnen
+        hoursFinal +=  (closingTime -openingTime)* daysBetween;
+        hoursFinal += startTime - openingTime;
+        hoursFinal += endTime - openingTime;
+
+        //TODO subtract weekends
+        
+    }
+
+    
+
+    return(hoursFinal);
+
+}
