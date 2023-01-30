2438252f92d61d045c957b6d5a26768c103e57da
derech1e <39240736+derech1e@users.noreply.github.com>
2023-01-30 16:51:48 +0100

    Move "Buchung" into second header postion

--- core/components/layout/HeaderComponent.tsx
+++ core/components/layout/HeaderComponent.tsx
@@ -12,9 +12,9 @@ export default function HeaderComponent() {
 
     const links = [
         {name: "Home", path: '#'},
+        {name: "Buchung", path: 'wizard'},
         {name: "Support", path: 'support'},
-        {name: "Hardware", path: 'hardware'},
-        {name: "Buchung", path: 'wizard'}
+        {name: "Hardware", path: 'hardware'}
     ];
 
     function toggleMenu(){
